1db1627d76f19a9da4865bf3b32f8da3b70348bf
JDelgado94 <169083882+JDelgado94@users.noreply.github.com>
2024-10-04 20:22:14 -0400

    Update Library.java
    Added Check in and out methods.
    Changed Remove book to two different methods: Remove by Barcode and Title. 
    Removed Read file method.

--- Library.java
+++ Library.java
@@ -1,6 +1,6 @@
 /*Project: Library Management System
 Name: Jorge Delgado
-Date: 9/1/2024
+Date: 9/27/2024
 Course: CEN:3024
 Class Name: Library Class
 Description: This class will contain all the book objects,
@@ -8,6 +8,8 @@ Description: This class will contain all the book objects,
              1. List all books in the collection
              2. Add books
              3. Remove books
+             4. Check in Books
+             5. Check out Books
 */
 
 import java.io.*;
@@ -32,27 +34,45 @@ public class Library {
      * purpose: Adds a book to the Library
      */
     public void addBook(Book book) {
-
         books.add(book);
     }
 
     //----------------------------------------------------------------------------------------
     /*
-     * method: removeBook
-     * parameters: bookID
+     * method: removeBookByBarcode
+     * parameters: barcode
      * return: void
-     * purpose: Removes a book from the collection by using the bookID
+     * purpose: Removes a book from the collection by using the book's barcode
      */
-    public void removeBook(int id) {
+    public void removeBookByBarcode(String barcode) {
         for(int i = 0; i < books.size(); i++) {
-            if(books.get(i).getBookId() == id) {
+            if(books.get(i).getBarcode().equals(barcode)) {
+                System.out.println( books.get(i).getTitle()+ " has been removed");
                 books.remove(i);
                 return;
             }
         }
-
+        System.out.println("Invalid choice, please try again later.");
     }
     //----------------------------------------------------------------------------------------
+    /*
+     * method: removeBookByTitle
+     * parameters: title
+     * return: void
+     * purpose: Removes a book from the collection by using the book's title
+     */
+    public void removeBookByTitle(String Title){
+        for(int i = 0; i < books.size(); i++) {
+            if(books.get(i).getTitle().equals(Title)) {
+                System.out.println( books.get(i).getTitle()+ " has been removed");
+                books.remove(i);
+                return;
+            }
+        }
+        System.out.println("Invalid choice, please try again later.");
+    }
+    //--
+    // --------------------------------------------------------------------------------------
     /*
      * method: listAllBooks
      * parameters: None
@@ -65,41 +85,63 @@ public class Library {
         } else {
             System.out.println("Books in library:");
             for (Book book : books) {
-                System.out.println(book.getBookId()+ "," + book.getTitle() + "," + book.getAuthor());
+                System.out.println(book.getBarcode()+ "," + book.getTitle() + "," + book.getAuthor()
+                                     + "," + book.getGenre() + "," + book.getStatus() + "," + book.getDueDate());
             }
         }
     }
 
     //----------------------------------------------------------------------------------------
     /*
-     * method: readFile
-     * parameters: None
+     * method: checkOutBook
+     * parameters: title
      * return: void
-     * purpose: Takes all the book info from the Library.txt file and creates
-     *          book objects
+     * purpose: Takes a book title and checks to see if it's available
+     *          if it is, this method changes the Book status to "Checked Out"
      */
+    public void checkOutBook(String title) {
+        for (Book book : books) {
+            if(book.getTitle().equals(title)) {
+                if(book.getStatus().equals("Available")) {
+                    book.setStatus("Checked Out");
+                    System.out.println(book.getTitle() + " is checked out.");
 
-    public void readFile(){
-        try {
-            File file = new File("Library.txt");
-            Scanner scanner = new Scanner(file);
-
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String [] splitter = line.split(",");
-                if (splitter.length == 3) {
-                    int id = Integer.parseInt(splitter[0].trim());
-                    String title = splitter[1].trim();
-                    String author = splitter[2].trim();
-                    Book book = new Book(id, title, author);
-                    books.add(book);
+                } else{
+                    System.out.println("Unfortunately, " + book.getTitle() + " book is not available");
                 }
+                return;
 
             }
-            scanner.close();
-        } catch (IOException e) {
-            System.out.println("File 'Library.txt' was not found");
         }
     }
 
+
+    //----------------------------------------------------------------------------------------
+    /*
+     * method: checkInBook
+     * parameters: title
+     * return: void
+     * purpose: Takes a book title checks to see if it is checked out and
+     *          if it is, this method changes the Book status to "Available"
+     */
+       public void checkInBook(String title) {
+           for (Book book : books) {
+               if(book.getTitle().equals(title)) {
+                   if(book.getStatus().equals("Checked Out")) {
+                       book.setStatus("Available");
+                       System.out.println(book.getTitle() + " is checked in.");
+                   }
+                   else{
+                       System.out.println("Book is already checked in.");
+                   }
+               }
+           }
+       }
+
+
+    //----------------------------------------------------------------------------------------
+
+
+
+}// End of Library Class
 }// End of Library Class
