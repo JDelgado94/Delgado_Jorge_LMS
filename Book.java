e4341a05a26100b4ffd9c4363ed4e71d5b5c1ed6
JDelgado94 <169083882+JDelgado94@users.noreply.github.com>
2024-10-04 20:16:04 -0400

    Update Book.java
    Updated variables

--- Book.java
+++ Book.java
@@ -1,27 +1,34 @@
 /*
 Project: Library Management System
 Name: Jorge Delgado
-Date: 9/1/2024
+Date: 9/27/2024
 Course: CEN:3024
 Class Name: Book Class
 Description: This class will create a book object with a
 Book ID, Title, and Author
 */
 public class Book {
-    private int bookId;
+    private String barcode;
     private String title;
     private String author;
+    private String genre;
+    private String status;
+    private String dueDate;
 
     // Constructor method:
-    public Book(int bookId, String title, String author) {
-        this.bookId = bookId;
+    public Book(String barcode, String title, String author, String genre, String status, String dueDate) {
+        this.barcode = barcode;
         this.title = title;
         this.author = author;
+        this.genre = genre;
+        this.status = status;
+        this.dueDate = dueDate;
+
     }
     //----------------------------------------------------------------------------------------
     // Getter Methods:
-    public int getBookId() {
-        return bookId;
+    public String getBarcode() {
+        return barcode;
     }
     public String getTitle(){
         return title;
@@ -29,11 +36,21 @@ public class Book {
     public String getAuthor(){
         return author;
     }
+    public String getStatus(){
+        return status; 
+    }
+    public String getGenre(){
+        return genre; 
+    }
+    public String getDueDate(){
+        return dueDate;
+    }
 //----------------------------------------------------------------------------------------
 
     // Setter methods:
-    public void setBookId(int bookId) {
-        this.bookId = bookId;
+    public void setBarcode(String barcode) {
+        
+        this.barcode = barcode;
     }
     public void setTitle(String title) {
         this.title = title;
@@ -41,6 +58,14 @@ public class Book {
     public void setAuthor(String author) {
         this.author = author;
     }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;}
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;}
     //----------------------------------------------------------------------------------------
 } // end of Book Class
+
 
