bfa2ec0396be845d417da476a0ba0de9662839d1
JDelgado94 <169083882+JDelgado94@users.noreply.github.com>
2024-11-16 19:39:43 -0500

    Update Main.java
    Updated for library database functionality

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 /*
 Project: Library Management System
 Name: Jorge Delgado
-Date: 10/29/2024
+Date: 11/08/2024
 Course: CEN:3024
 App Name: LMS.java
-Description: This application will allow the user to
-add, remove and view a collection books from a text file
+Description: This application will allow the user to connect to a library
+ database and add, remove, and check in/out books that are in the library.
 */
 
 import javax.swing.*;
@@ -18,7 +18,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //Starts GUI Operation:
-        FileInput fileInput = new FileInput();
+        Library lib = new Library();
+        MainMenu mainMenu = new MainMenu(lib);
 
     }
 
