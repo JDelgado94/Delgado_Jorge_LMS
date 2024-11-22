ffb9620d70999ff2edce9aa556b18524f3940904
JDelgado94 <169083882+JDelgado94@users.noreply.github.com>
2024-11-22 12:49:13 -0500

    Update Main.java
    Added Javadoc comments

--- Main.java
+++ Main.java
@@ -1,11 +1,7 @@
-/*
-Project: Library Management System
+/*Project: Library Management System
 Name: Jorge Delgado
 Date: 11/08/2024
 Course: CEN:3024
-App Name: LMS.java
-Description: This application will allow the user to connect to a library
- database and add, remove, and check in/out books that are in the library.
 */
 
 import javax.swing.*;
@@ -13,11 +9,29 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/**
+ * <h1>Library Management System</h1>
+ *
+ * Description -- This application will allow the user to connect to a library
+ *              database and add, remove, and check in/out books that are in
+ *              the library.
+ *
+ * @author -- Jorge Delgado
+ * @version -- 1.3
+ */
 
-//Main Method:
+
 public class Main {
+
+    /**
+     *
+     * Main -- This method starts the GUI LMS application.
+     * @param args -- Unused
+     *
+     */
+
     public static void main(String[] args) {
-       
+
         Library lib = new Library();
         MainMenu mainMenu = new MainMenu(lib);
 
